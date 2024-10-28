36bcc46980eadaf93cc6cab03696722b817e62c2
Ly Thanh Tu Anh <tuanhlythanh@gmail.com>
2024-10-28 14:09:37 +0700

    first commit

---
+++ backend/TestTH/Data/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using TestTH.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTH.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions<ProductContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
